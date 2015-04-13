5aab6543221ca419fffcda59f8fb99421625369a
Tomas Aparicio <tomas@aparicio.me>
2015-04-13 09:56:54 +0200

    feat(#16): add benchmark

---
+++ bench.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Simple benchmark test suite
+#
+# You must have installed vegeta:
+# go get github.com/tsenart/vegeta
+#
+
+host="http://localhost:8088"
+
+echo "Resize -----------------------------------"
+echo "POST $host/resize?width=300" | vegeta attack \
+  -duration=10s \
+  -rate=30 \
+  -body="./fixtures/large.jpg" \ | vegeta report
+
+echo 
+
+echo "Extract -------------------------------------"
+echo "POST $host/extract?top=50&left=50&areawidth=200&areaheight=200" | vegeta attack \
+  -duration=10s \
+  -rate=30 \
+  -body="./fixtures/large.jpg" \ | vegeta report
+
+echo
+
+echo "Rotate -------------------------------------"
+echo "POST $host/rotate?rotate=180" | vegeta attack \
+  -duration=10s \
+  -rate=30 \
+  -body="./fixtures/large.jpg" \ | vegeta report
