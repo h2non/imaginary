3e2d8179d2aa22d6206c821e970bbabe3409a018
Tomas Aparicio <tomas@aparicio.me>
2016-02-20 11:59:50 +0000

    feat(#62): support allowed origins

--- source_http.go
+++ source_http.go
@@ -22,41 +22,56 @@ func (s *HttpImageSource) Matches(r *http.Request) bool {
 }
 
 func (s *HttpImageSource) GetImage(req *http.Request) ([]byte, error) {
-	url, err := s.parseURL(req)
+	url, err := parseURL(req)
 	if err != nil {
 		return nil, ErrInvalidImageURL
+	}
+	if shouldRestrictOrigin(url, s.Config.AllowedOrigings) {
+		return nil, fmt.Errorf("Not allowed remote URL origin: %s", url.Host)
 	}
 	return s.fetchImage(url)
 }
 
 func (s *HttpImageSource) fetchImage(url *url.URL) ([]byte, error) {
-	req := s.newHTTPRequest(url)
+	req := newHTTPRequest(url)
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("Error downloading image: %v", err)
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("Error downloading image: (status=%d) (url=%s)", res.StatusCode, req.URL.RequestURI())
+		return nil, fmt.Errorf("Error downloading image: (status=%d) (url=%s)", res.StatusCode, req.URL.String())
 	}
 
 	buf, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to create image from response body: %s (url=%s)", req.URL.RequestURI(), err)
+		return nil, fmt.Errorf("Unable to create image from response body: %s (url=%s)", req.URL.String(), err)
 	}
 	return buf, nil
 }
 
-func (s *HttpImageSource) parseURL(request *http.Request) (*url.URL, error) {
+func parseURL(request *http.Request) (*url.URL, error) {
 	queryUrl := request.URL.Query().Get("url")
 	return url.Parse(queryUrl)
 }
 
-func (s *HttpImageSource) newHTTPRequest(url *url.URL) *http.Request {
-	req, _ := http.NewRequest("GET", url.RequestURI(), nil)
-	req.Header.Set("User-Agent", "imaginary")
+func newHTTPRequest(url *url.URL) *http.Request {
+	req, _ := http.NewRequest("GET", url.String(), nil)
+	req.Header.Set("User-Agent", "imaginary/"+Version)
 	req.URL = url
 	return req
+}
+
+func shouldRestrictOrigin(url *url.URL, origins []*url.URL) bool {
+	if len(origins) == 0 {
+		return false
+	}
+	for _, origin := range origins {
+		if origin.Host == url.Host {
+			return false
+		}
+	}
+	return true
 }
 
 func init() {
