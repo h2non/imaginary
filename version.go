839b474b474290ecbc71cb9c449c1a484dd1b7e6
Tomas Aparicio <tomas@aparicio.me>
2017-01-18 22:41:48 +0000

    refacgor(version): add comments

--- version.go
+++ version.go
@@ -2,12 +2,15 @@ package main
 
 import "gopkg.in/h2non/bimg.v1"
 
+// Version stores the current package semantic version
 const Version = "0.1.30"
 
+// Version represents the supported version
 type Versions struct {
 	ImaginaryVersion string `json:"imaginary"`
 	BimgVersion      string `json:"bimg"`
 	VipsVersion      string `json:"libvips"`
 }
 
+// CurrentVersions stores the current runtime system version metadata
 var CurrentVersions = Versions{Version, bimg.Version, bimg.VipsVersion}
