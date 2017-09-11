75baba3fe6764827c4c2dfc37028bc5f4f07a5a8
Tomas Aparicio <tomas@aparicio.me>
2017-09-11 16:39:51 +0200

    feat(version): bump to v1.0.6

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package main
 import "gopkg.in/h2non/bimg.v1"
 
 // Version stores the current package semantic version
-const Version = "1.0.5"
+const Version = "1.0.6"
 
 // Version represents the supported version
 type Versions struct {
