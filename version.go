c75d2bacfbf152a840458acb9cdde29263c0aae9
Mark van der Velden <mark@dynom.nl>
2019-01-20 16:29:56 +0100

    Bumping version to 1.0.17

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package main
 import "gopkg.in/h2non/bimg.v1"
 
 // Version stores the current package semantic version
-const Version = "1.0.15"
+const Version = "1.0.17"
 
 // Versions represents the used versions for several significant dependencies
 type Versions struct {
