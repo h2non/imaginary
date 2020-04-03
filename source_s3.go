e23794ccf95a1e93c54f2097bbee6f1507390def
Antonio Bosak <toni.bosak@gmail.com>
2020-04-03 12:21:48 +0200

    added secret through env var + refactor

--- source_s3.go
+++ source_s3.go
@@ -3,138 +3,101 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"math/rand"
-	"net/http"
-	"os"
-	"strings"
-	"time"
 )
 
-const ImageSourceTypeS3 = "s3"
-const S3Key = "s3key"
-const S3OutputKey = "outputKey"
-const S3Bucket = "bucket"
-const S3Region = "region"
+const ImageSourceTypeS3 ImageSourceType = "s3"
+
+func init() {
+	RegisterSource(ImageSourceTypeS3, NewS3ImageSource)
+}
+
+func newS3Session(region string) *session.Session {
+	// TODO: if we are ever to migrate to azure this should be made that is fails silently
+	// not with panic
+	return session.Must(session.NewSession(&aws.Config{
+		Region: &region,
+		Credentials: credentials.NewStaticCredentials(
+			os.Getenv("S3_KEY"),
+			os.Getenv("S3_KEY_SECRET"),
+			"",
+		),
+	}))
+}
 
 type S3ImageSource struct {
 	Config *SourceConfig
 }
 
 func NewS3ImageSource(config *SourceConfig) ImageSource {
-	return &S3ImageSource{config}
+	return &S3ImageSource{Config: config}
 }
 
 func (s *S3ImageSource) Matches(r *http.Request) bool {
-	return r.Method == http.MethodGet && r.URL.Query().Get(S3Key) != ""
+	return r.Method == http.MethodGet && parseKey(r) != ""
 }
 
 func (s *S3ImageSource) GetImage(req *http.Request) ([]byte, error) {
+	key, bucket, region := parseKey(req), parseBucket(req), parseS3Region(req)
 
-	key := parseKey(req)
-	bucket := parseBucket(req)
-	region := parseS3Region(req)
+	fmt.Printf(
+		"getImage S3 - key: %s, bucket: %s, region: %s\n",
+		key, bucket, region,
+	)
 
-	fmt.Print("getImage S3 - key: " + key + " bucket: "+bucket + " region: "+region)
-
-	sess := createSession(region)
-
-	downloader := s3manager.NewDownloader(sess)
-	filename := "/tmp/" + randString() + ".png"
-
-	f, errFile := os.Create(filename)
-	if errFile != nil {
-		_ = fmt.Errorf("failed to create file, %v", errFile)
+	buffer := aws.NewWriteAtBuffer([]byte{})
+	buffer.GrowthCoeff = 1.5
+	if _, err := s3manager.NewDownloader(newS3Session(region)).
+		Download(
+			buffer,
+			&s3.GetObjectInput{
+				Bucket: aws.String(bucket),
+				Key:    &key,
+			}); err != nil {
+		return nil, fmt.Errorf("failed to download file, %w", err)
 	}
 
-	// Write the contents of S3 Object to the file
-	n, err := downloader.Download(f, &s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    &key,
-	})
-	if err != nil {
-		_ = fmt.Errorf("failed to download file, %v", err)
-	}
-	fmt.Printf(" file downloaded, %d bytes\n", n)
+	fmt.Printf("file downloaded, %d bytes, array cap %d\n",
+		len(buffer.Bytes()), cap(buffer.Bytes()),
+	)
 
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-
-	fileinfo, err := file.Stat()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	filesize := fileinfo.Size()
-	buffer := make([]byte, filesize)
-
-	_, err = file.Read(buffer)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return buffer, err
+	return buffer.Bytes(), nil
 }
 
-func createSession(region string) *session.Session{
-	profile := "default"
-	path := "/go/config/credentials"
-
-	sess := session.Must(session.NewSession(&aws.Config{
-		Region:      &region,
-		Credentials: credentials.NewSharedCredentials(path, profile),
-	}))
-	return sess
-}
-
-func uploadBufferToS3(buffer []byte, outputKey string, bucket string, region string){
-	sess := createSession(region)
+func uploadBufferToS3(buffer []byte, outputKey, bucket, region string) error {
+	sess := newS3Session(region)
 	uploader := s3manager.NewUploader(sess)
-	// Upload the file to S3.
-	result, err := uploader.Upload(&s3manager.UploadInput{
+
+	if _, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucket),
 		Key:    &outputKey,
 		Body:   bytes.NewReader(buffer),
-	})
-	if err != nil {
-		_ = fmt.Errorf("failed to upload file, %v ", err)
+	}); err != nil {
+		return fmt.Errorf("failed to upload file, %w", err)
 	}
-	fmt.Print("upload successful to "+result.Location + " ")
+
+	return nil
 }
 
-
-
-func parseKey(request *http.Request) (string) {
-	return request.URL.Query().Get(S3Key)
+func parseKey(request *http.Request) string {
+	return request.URL.Query().Get("s3key")
 }
 
-func parseBucket(request *http.Request) (string) {
-	return request.URL.Query().Get(S3Bucket)
+func parseOutputKey(request *http.Request) string {
+	return request.URL.Query().Get("outputKey")
 }
 
-func parseS3Region(request *http.Request) (string) {
-	return request.URL.Query().Get(S3Region)
+func parseBucket(request *http.Request) string {
+	return request.URL.Query().Get("bucket")
 }
 
-func init() {
-	RegisterSource(ImageSourceTypeS3, NewS3ImageSource)
-}
-
-func randString() string {
-	rand.Seed(time.Now().UnixNano())
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-		"abcdefghijklmnopqrstuvwxyz" +
-		"0123456789")
-	length := 8
-	var b strings.Builder
-	for i := 0; i < length; i++ {
-		b.WriteRune(chars[rand.Intn(len(chars))])
-	}
-	return b.String() // E.g. "ExcbsVQs"
+func parseS3Region(request *http.Request) string {
+	return request.URL.Query().Get("region")
 }
