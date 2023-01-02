550f00ff592801d2990b91dd357a6e6fdea03b04
Kian-Meng Ang <kianmeng@cpan.org>
2023-01-02 14:40:52 +0800

    Fix typos
    Found via `codespell -S .git`.

--- error.go
+++ error.go
@@ -19,7 +19,7 @@ var (
 	ErrEmptyBody            = NewError("Empty or unreadable image", http.StatusBadRequest)
 	ErrMissingParamFile     = NewError("Missing required param: file", http.StatusBadRequest)
 	ErrInvalidFilePath      = NewError("Invalid file path", http.StatusBadRequest)
-	ErrInvalidImageURL      = NewError("Unvalid image URL", http.StatusBadRequest)
+	ErrInvalidImageURL      = NewError("Invalid image URL", http.StatusBadRequest)
 	ErrMissingImageSource   = NewError("Cannot process the image due to missing or invalid params", http.StatusBadRequest)
 	ErrNotImplemented       = NewError("Not implemented endpoint", http.StatusNotImplemented)
 	ErrInvalidURLSignature  = NewError("Invalid URL signature", http.StatusBadRequest)
